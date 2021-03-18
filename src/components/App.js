24e0371b770ac32f9ff87c608c8edc3217397274
William Bugenis <wbugenis@gmail.com>
2021-03-17 22:46:08 -0400

    commit tree e0470d0ddb09758e30286967ab6e22d3ccc44d1d

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
-
+  const [isDark, setDark] = useState(false)
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (isDark ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={()=>setDark(!isDark)}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterState, setFilter] = useState("All")
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filterItems = () => {
+    if (filterState === "All") {
+      return items
+    } else {
+      return items.filter(item => item.category === filterState)
+    } 
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={filterState} onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterItems().map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
